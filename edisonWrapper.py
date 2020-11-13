3f2253d02ed6becef5c57a49e5760edbd99255d2
RMiller <acemiller6@gmail.com>
2020-11-12 21:52:13 -0600

    Initial upload of Edison JSON data and initial python scripts

---
+++ edisonWrapper.py
@@ -0,0 +1,72 @@
+import argparse
+from argparse import RawDescriptionHelpFormatter
+from fraudcatch import findfraud, findfraud2
+
+
+description = "Parse Edison Json data from 2020 election for all states.  Full list of states to use on the command line (note names are not always complete):\n\
+    alabama\n\
+    alaska\n\
+    arizona\n\
+    arkansas\n\
+    california\n\
+    colorado\n\
+    connecticut\n\
+    delaware\n\
+    districtofcolumbia\n\
+    florida\n\
+    georgia\n\
+    idaho\n\
+    illinois\n\
+    indiana\n\
+    iowa\n\
+    jersey\n\
+    kansas\n\
+    kentucky\n\
+    louisiana\n\
+    maine\n\
+    maryland\n\
+    massachusetts\n\
+    mexico\n\
+    michigan\n\
+    minnesota\n\
+    mississippi\n\
+    missouri\n\
+    montana\n\
+    nebraska\n\
+    nevada\n\
+    newhamp\n\
+    newyork\n\
+    northcarolina\n\
+    northdakota\n\
+    ohio\n\
+    oklahoma\n\
+    oregon\n\
+    pennsylvania\n\
+    rhode\n\
+    southcarolina\n\
+    southdakota\n\
+    tenn\n\
+    texas\n\
+    utah\n\
+    vermont\n\
+    virginia\n\
+    washington\n\
+    westvirginia\n\
+    wisconsin\n\
+    wyoming\n"
+
+parser = argparse.ArgumentParser(description=description, formatter_class=RawDescriptionHelpFormatter)
+parser.add_argument('state', help='state name to look at data (see list above)')
+args = parser.parse_args()
+
+# stateList = ["alabama","alaska","arizona","arkansas","california","colorado","connecticut","delaware","districtofcolumbia","florida","georgia","idaho","illinois","indiana",\
+#     "iowa","jersey","kansas","kentucky","louisiana","maine","maryland","massachusetts","mexico","michigan","minnesota","mississippi","missouri","montana","nebraska","nevada",\
+#         "newhamp","newyork","northcarolina","northdakota","ohio","oklahoma","oregon","pennsylvania","rhode","southcarolina","southdakota","tenn","texas","utah","vermont",\
+#             "virginia","washington","westvirginia","wisconsin","wyoming"]
+# for state in stateList:
+#     switch = findfraud(state)
+#     lost = findfraud2(state)
+#     print("{},{},{}".format(state,switch,lost))
+
+findfraud(args.state)
+findfraud2(args.state)
---
+++ fraudcatch.py
@@ -0,0 +1,29 @@
+import json
+
+
+
+def findfraud(NAME):
+    with open(NAME + '.json', encoding="utf8") as f:
+        x = json.load(f)
+    TotalVotesLost = 0
+    print("Total vote updates: {}".format(len(x["data"]["races"][0]["timeseries"])))
+    for i in range(len(x["data"]["races"][0]["timeseries"])):
+        if i != 0 and x["data"]["races"][0]["timeseries"][i]["votes"] * x["data"]["races"][0]["timeseries"][i]["vote_shares"]["trumpd"] < x["data"]["races"][0]["timeseries"][i-1]["votes"] * x["data"]["races"][0]["timeseries"][i-1]["vote_shares"]["trumpd"]:
+            if x["data"]["races"][0]["timeseries"][i]["votes"] * x["data"]["races"][0]["timeseries"][i]["vote_shares"]["bidenj"] > x["data"]["races"][0]["timeseries"][i-1]["votes"] * x["data"]["races"][0]["timeseries"][i-1]["vote_shares"]["bidenj"]:
+                print ("Timestamp: {}".format(x["data"]["races"][0]["timeseries"][i]["timestamp"]))
+                print ("    Index : " + str(i) + " < " + str(i-1))
+                print ("    Votes switched: {}".format(x["data"]["races"][0]["timeseries"][i]["votes"] * x["data"]["races"][0]["timeseries"][i]["vote_shares"]["trumpd"] - x["data"]["races"][0]["timeseries"][i-1]["votes"] * x["data"]["races"][0]["timeseries"][i-1]["vote_shares"]["trumpd"]))
+                TotalVotesLost += x["data"]["races"][0]["timeseries"][i]["votes"] * x["data"]["races"][0]["timeseries"][i]["vote_shares"]["trumpd"] - x["data"]["races"][0]["timeseries"][i-1]["votes"] * x["data"]["races"][0]["timeseries"][i-1]["vote_shares"]["trumpd"]
+    print ("Total Votes Switched: {}\n".format(TotalVotesLost))
+    return TotalVotesLost
+
+def findfraud2(NAME):
+    with open(NAME + '.json', encoding="utf8") as f:
+        x = json.load(f)
+    TotalVotesLost = 0
+    for i in range(len(x["data"]["races"][0]["timeseries"])):
+        if i != 0 and x["data"]["races"][0]["timeseries"][i]["votes"] < x["data"]["races"][0]["timeseries"][i-1]["votes"]:
+            TotalVotesLost += x["data"]["races"][0]["timeseries"][i]["votes"] - x["data"]["races"][0]["timeseries"][i-1]["votes"]
+            print("index where Votes lost: {}".format(i))
+    print("Total Votes Lost: {}".format(TotalVotesLost))
+    return TotalVotesLost
